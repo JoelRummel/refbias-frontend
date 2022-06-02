d32df4a293bedaa4ef590bee1971de69766a813d
joelrummel <joelrummel@yahoo.com>
2022-06-02 01:30:59 -0400

    Major facelift to games listing

--- src/routes/Games.jsx
+++ src/routes/Games.jsx
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import teamLogos from '../config/teamLogos';
 import { teamsMap } from '../config/teams';
 import useGamesApiRequest from '../hooks/useApiRequest/useGamesApiRequest';
 import { readableDateDiff } from '../util/dates';
 import { gameToQueryString } from '../util/gameQueryString';
 
 const GameCard = styled.div`
-    border: 2px solid black;
-    padding: 10px;
+    border: 2px solid gray;
     margin: 10px;
     border-radius: 5px;
+    display: flex;
+    align-items: center;
+    min-height: 100px;
+`;
+
+const SideContainer = styled.div`
+    display: flex;
+    position: relative;
+    flex-grow: 1;
+    align-self: stretch;
+    align-items: center;
+`;
+
+const Gradient = styled.div`
+    position: absolute;
+    left: ${({ side }) => side === 'home' ? '-20px' : 0};
+    right: ${({ side }) => side === 'away' ? '-20px' : 0};
+    top: 0;
+    bottom: 0;
+    z-index: -1;
+    background: linear-gradient(to ${({ side }) => side === 'away' ? 'right' : 'left'}, ${({ color }) => color} 0%, ${({ color }) => color}50 50%, rgba(0,0,0,0) 100%);
+`;
+
+const TeamLogo = styled.img`
+    width: 75px;
+    height: 75px;
 `;
 
 const Game = ({ game }) => {
@@ -25,10 +51,20 @@ const Game = ({ game }) => {
     if (status === 'upcoming') statusText = `Closed - voting starts in ${readableDateDiff(now, voteOpenTime)}`;
 
     return (
-        <Link to={`/game/${id}?${gameToQueryString(game)}`}>
+        <Link to={`/game/${id}?${gameToQueryString(game)}`} style={{ color: 'unset' }}>
             <GameCard>
-                <p>{awayInfo.mascot} at {homeInfo.mascot}</p>
-                <p>Voting status: {statusText}</p>
+                <SideContainer>
+                    <Gradient side="away" color={awayInfo.colors.primary} />
+                    <TeamLogo src={teamLogos[away]} style={{ marginLeft: 15 }} />
+                </SideContainer>
+                <div style={{ flexGrow: 1 }}>
+                    <h3 style={{ textAlign: 'center' }}>{awayInfo.mascot} at {homeInfo.mascot}</h3>
+                    <p style={{ textAlign: 'center' }}>Status: {statusText}</p>
+                </div>
+                <SideContainer style={{ justifyContent: 'flex-end' }}>
+                    <Gradient side="home" color={homeInfo.colors.primary} />
+                    <TeamLogo src={teamLogos[home]} style={{ marginRight: 15 }} />
+                </SideContainer>
             </GameCard>
         </Link>
     );
